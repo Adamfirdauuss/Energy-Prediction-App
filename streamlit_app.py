c29532d57fa9951d9f80ad9e1996e37030328bf8
Adamfirdauuss <adamfirdauuss@gmail.com>
2025-04-13 18:01:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Enegy Prediction App')
 
-st.write('Hello world!')
+st.info('This App....')
+
+df = pd.read_csv('https://raw.githubusercontent.com/Adamfirdauuss/Energy-Prediction-App/refs/heads/master/power%20Generation%20and%20consumption.csv')
+df
